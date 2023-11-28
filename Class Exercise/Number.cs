a4cb67759b1714c31c412dea46e19be6dc36d54d
Lauren <laurenleighpetrus3@gmaiil.com>
2023-11-28 12:12:19 +0200

    Initial Commit

---
+++ Class Exercise/Number.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Exercise
+{
+    internal class Number
+    {
+
+
+        public static int SecondbiggestNumber(int[] numbers)
+        {
+            int secondlast = BiggestNumber(numbers);
+            int answer = int.MinValue;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if ((numbers[i] > answer) && numbers[i] < secondlast)
+                    answer = numbers[i];
+            }
+
+            return answer;
+
+
+
+        }
+
+        public static int BiggestNumber(int[] numbers)
+
+        {
+            int lastbiggestnumber = int.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (numbers[i] > lastbiggestnumber)
+                {
+                    lastbiggestnumber = numbers[i];
+
+                }
+
+
+
+
+
+
+            }
+
+            return lastbiggestnumber;
+        }
+
+           
+
+
+     }
+
+}
+
+            
+
+        
+    
---
+++ Class Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Class_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mynumbers = { 2, 1, 7, 4, 9, 5 };
+
+            int biggest = Number.BiggestNumber(mynumbers);
+
+            Console.WriteLine("The Biggest number is" + biggest);
+
+            int secondbiggest = Number.SecondbiggestNumber(mynumbers);
+            Console.WriteLine("The Second Biggest Number is" + secondbiggest);
+        }
+
+    }
+}
